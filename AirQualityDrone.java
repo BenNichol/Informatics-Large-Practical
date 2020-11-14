539ee96adce9ea1bb621ac94eea11e9e6d3da5a4
BenNichol <71698784+BenNichol@users.noreply.github.com>
2020-11-14 12:35:05 +0000

    Add files via upload

--- AirQualityDrone.java
+++ AirQualityDrone.java
@@ -1,4 +1,4 @@
-package uk.ac.ed.inf.drone;
+package uk.ac.ed.inf.aqmaps;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,10 +9,15 @@ import java.util.List;
 import com.mapbox.geojson.Geometry;
 import com.mapbox.geojson.Point;
 import com.mapbox.geojson.Polygon;
+import com.mapbox.geojson.LineString;
 
 import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.geojson.Feature;
 import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -24,15 +29,17 @@ import com.google.gson.reflect.TypeToken;
 
 public class AirQualityDrone
 {
-    public static void main( String[] args ) throws IOException
+    public static void main( String[] args ) throws IOException, InterruptedException
     {
-    	//System.out.print((Math.toDegrees(Math.atan2(-1, -1))+360) % 360);
         Drone drone = new Drone();
+        var allFeatures = new ArrayList<List<Feature>>();
         FeatureCollection nfz = noFlyZones(args);
         ArrayList<SensorsList> sensors = dailySensors(args);
         ArrayList<Point> coordinates = what3words(args, sensors);
         ArrayList<Integer> dronePath = greedyPath(drone, coordinates, args);
-        flightPath(drone, sensors, dronePath, coordinates);
+        var sensorOutput = flightPath(drone, sensors, dronePath, coordinates, nfz, allFeatures);
+        var sensorMap = hexCodeConversion(sensorOutput);
+        geojsonConvert(sensorMap, sensors, dronePath, coordinates);
     }
     
     public static class Drone 
@@ -79,67 +86,57 @@ public class AirQualityDrone
     	String language;
     	String map;
     }
+   
+    // Just have one HttpClient, shared between all HttpRequests
+    private static final HttpClient client = HttpClient.newHttpClient();
     
-    public static FeatureCollection noFlyZones(String[] args) throws IOException
+    
+    public static FeatureCollection noFlyZones(String[] args) throws IOException, InterruptedException
     // This method will load in the "No Fly Zones" from the drone and return them as a Feature Collection
     {
     	// Load in the "No Fly Zones" for the drone
-    	String no_fly_zone = "D:\\User Folders\\Ben\\Desktop\\Coding\\Year 3\\Informatics Large Practical\\Coursework 2\\drone\\WebServer\\buildings\\no-fly-zones.geojson";
-    	var fr = new FileReader(no_fly_zone);
-    	int reader1;
-    	String nfz = "";
-    	while ((reader1=fr.read()) != -1)
-        {
-        	nfz = nfz + (char) reader1;
-        }
-    	fr.close();
-    	return FeatureCollection.fromJson(nfz);
+    	// HttpClient assumes that it is a GET request by default.
+    	String urlString = "http://localhost:" + args[6] + "/buildings/no-fly-zones.geojson";
+    	var request = HttpRequest.newBuilder().uri(URI.create(urlString)).build();
+    	// The response object is of class HttpResponse<String>
+    	var response = client.send(request, BodyHandlers.ofString());
+    	var nfz = FeatureCollection.fromJson(response.body());
+    	return nfz;
     }
     
-    public static ArrayList<SensorsList> dailySensors(String[] args) throws IOException
+    public static ArrayList<SensorsList> dailySensors(String[] args) throws IOException, InterruptedException
     // This method will load the list of sensors to be visited by the drone and return
     // them as an Array List of type SensorsList (as parsed by the Gson().fromJson function)
     {	
     	// Load in the list of sensors for that day
-    	String air_quality_data = "D:\\User Folders\\Ben\\Desktop\\Coding\\Year 3\\Informatics Large Practical\\Coursework 2\\drone\\WebServer\\maps\\" + args[2] + "\\"
-    							  + args[1] + "\\" + args[0] + "\\air-quality-data.json";
-    	var fr2 = new FileReader(air_quality_data);
-    	int reader2;
-    	String aqd = "";
-    	while ((reader2=fr2.read()) != -1)
-        {
-        	aqd = aqd + (char) reader2;
-        }
-    	fr2.close();
+    	String urlString = "http://localhost:" + args[6] + "/maps/" + args[2] + "/" + args[1] + "/" + args[0] + "/air-quality-data.json";
+    	var request = HttpRequest.newBuilder().uri(URI.create(urlString)).build();
+    	// The response object is of class HttpResponse<String>
+    	var response = client.send(request, BodyHandlers.ofString());
     	
     	Type listType = new TypeToken<ArrayList<SensorsList>>() {}.getType();
-    	ArrayList<SensorsList> sensors = new Gson().fromJson(aqd, listType);
+    	ArrayList<SensorsList> sensors = new Gson().fromJson(response.body(), listType);
     	
     	return sensors;
     }
     
-    public static ArrayList<Point> what3words(String[] args, ArrayList<SensorsList> sensors) throws IOException
+    public static ArrayList<Point> what3words(String[] args, ArrayList<SensorsList> sensors) throws InterruptedException, IOException
     {
     	// Load in all the sensor details from What3Words address
+    	
     	var coordinates = new ArrayList<Point>();
     	for(int i = 0; i < 33; i++) {
     		String location = sensors.get(i).location;
     		String[] what3words = location.split("\\.");
-
-    		String sensor_details = "D:\\User Folders\\Ben\\Desktop\\Coding\\Year 3\\Informatics Large Practical\\Coursework 2\\drone\\WebServer\\words\\" + what3words[0] + "\\"
-				  + what3words[1] + "\\" + what3words[2] + "\\details.json";
+    		String urlString = "http://localhost:" + args[6] + "/words/" + what3words[0] + "/" + what3words[1] + "/" + what3words[2] + "/details.json";
     		
-    		var fr3 = new FileReader(sensor_details);
-			String sd = "";
-			int reader3;
-			while ((reader3=fr3.read()) != -1)
-			{
-			sd = sd + (char) reader3;
-			}
-			fr3.close();
+    		var request = HttpRequest.newBuilder().uri(URI.create(urlString)).build();
+    		// The response object is of class HttpResponse<String>
+    		var response = client.send(request, BodyHandlers.ofString());
     		
-    		var sensorDetails = new Gson().fromJson(sd, SensorDetails.class);   
+    		var sensorDetails = new Gson().fromJson(response.body(), SensorDetails.class);   
     		coordinates.add(Point.fromLngLat(sensorDetails.coordinates.lng, sensorDetails.coordinates.lat));
+    		
     	}
     	return coordinates;
     }
@@ -189,27 +186,6 @@ public class AirQualityDrone
     		}
     	}
     	
-    	// Find out which sensor the drone is closest to
-    	//int droneClosest = 0;
-    	//for(int i = 0; i < 33; i++)
-    	//{
-    	//	if (euclidDist(drone.position, coordinates.get(i)) < euclidDist(drone.position, coordinates.get(droneClosest)))
-    	//	{
-    	//		droneClosest = i;
-    	//	}
-    	//}
-    	
-    	//for(int i = 0; i < sensorList.size(); i++)
-    	//{
-    	//	System.out.print(sensorList.get(i) + " ");
-    	//}
-    	// Shift the sensor list permutation so that the drone's closest sensor is the first element
-    	//var dronePath = new ArrayList<Integer>();
-    	//for(int i = 0; i < 33; i++)
-    	//{
-    	//	dronePath.add((sensorList.get((sensorList.indexOf(droneClosest)+i) % sensorList.size())));
-    	//	//System.out.print(dronePath.get(i) + " ");
-    	//}
     	// Append the drone's starting position to the end
     	dronePath.add(0);
     	
@@ -225,139 +201,188 @@ public class AirQualityDrone
     	return dist;
     }
     
-    public static void flightPath(Drone drone, ArrayList<SensorsList> sensors, ArrayList<Integer> dronePath, ArrayList<Point> coordinates)
+    //---------------------------------PILE OF WANK---------------------------------------
+    public static boolean insidePolygon(Point dronePos, FeatureCollection nfz)
+    // This method will determine whether the point is inside the polygon or not
+    {
+    	for(int i = 0; i < nfz.features().size(); i++)
+    	{
+    		Polygon poly = (Polygon)nfz.features().get(i).geometry();
+    		for(int j = 0; j < poly.coordinates().size(); j++)
+    		{
+    			
+    		}
+    	}
+    	
+    	
+    	
+    	return true;
+    }
+    //---------------------------------PILE OF WANK---------------------------------------
+    
+    
+    public static ArrayList<List<String>> flightPath(Drone drone, ArrayList<SensorsList> sensors, ArrayList<Integer> dronePath, ArrayList<Point> coordinates, FeatureCollection nfz, ArrayList<List<Feature>> allFeatures)
     // This method will control the drone's flight, taking it to the different sensors
     // and making sure it does not go into any restricted areas
     {
+    	// No fly zones
+    	var movementHistory = new ArrayList<Point>();
     	var readings = new ArrayList<String>();
-    	double hypotenuse = 0;
-    	double height = 0;
-    	double width = 0;
+    	var batteries = new ArrayList<String>();
+    	double deltaLng = 0;
+    	double deltaLat = 0;
     	boolean sensProx = false;
+    	Point newPosition;
     	for(int i = 1; i < dronePath.size(); i++)
     	{
     		sensProx = false;
     		while (!sensProx & drone.moves < 150)
 	    	{
+    			// Record all the positions of the drone
+    			movementHistory.add(drone.position);
+    			
 	    		// Determine angle of travel
-	    		hypotenuse = euclidDist(coordinates.get(dronePath.get(i)), drone.position);
-	    		height = coordinates.get(dronePath.get(i)).latitude() - drone.position.latitude();
-	    		width = coordinates.get(dronePath.get(i)).longitude() - drone.position.longitude();
-	    		drone.angle = (int) Math.round((Math.toDegrees(Math.atan2(height, width)))/10)*10;
+	    		deltaLat = coordinates.get(dronePath.get(i)).latitude() - drone.position.latitude();
+	    		deltaLng = coordinates.get(dronePath.get(i)).longitude() - drone.position.longitude();
+	    		drone.angle = (int) Math.round((Math.toDegrees(Math.atan2(deltaLat, deltaLng)))/10)*10;
 	    		
 	    		// Convert negative angles into their positive counterparts
 	    		drone.angle = ((drone.angle + 360) % 360);	    		
-	    		//System.out.print("\nClosest sensor = " + "[" + dronePath.get(i) + "] " + coordinates.get(dronePath.get(i))
-	    		//				 + "\nDistance from sensor = " + hypotenuse
-	    		//				 + "\nDrone angle = " + drone.angle);
 	    		
 	    		// Update the drone's position
-	    		drone.position = Point.fromLngLat(drone.position.longitude() + (Math.cos(Math.toRadians(drone.angle)) * 0.0003), drone.position.latitude() + (Math.sin(Math.toRadians(drone.angle)) * 0.0003));
-	    		//System.out.println("\nNew distance from sensor = " + euclidDist(coordinates.get(dronePath.get(i)), drone.position));
-	    		//System.out.println("\nCHECKING 0.0003 = " + Math.sqrt(Math.pow((Math.sin(drone.angle)*0.0003), 2) + Math.pow((Math.cos(drone.angle)*0.0003), 2)));
+	    		newPosition = Point.fromLngLat(drone.position.longitude() + (Math.cos(Math.toRadians(drone.angle)) * 0.0003), drone.position.latitude() + (Math.sin(Math.toRadians(drone.angle)) * 0.0003));
+	    		
+	    		drone.position = newPosition;
 	    		
 	    		// Increment the drone's total moves
 	    		drone.moves ++;
 	    		//System.out.println("\nTotal moves = " + drone.moves);
 	    		
 	    		// Check whether the drone is close enough to a sensor to read it, and if so, take a reading
-	    		// System.out.println("\n" + euclidDist(drone.position, coordinates.get(dronePath.get(i))));
 	    		if(euclidDist(drone.position, coordinates.get(dronePath.get(i))) <= 0.0002)
 	    		{
-	    			//System.out.println("\nThe drone is close enough for a reading at sensor " + dronePath.get(i));
-	    			readings.add(sensors.get(dronePath.get(i)).reading);
+	    			if(i < dronePath.size()-1)
+					{
+						readings.add(sensors.get(dronePath.get(i)-1).reading);
+						batteries.add(String.valueOf(sensors.get(dronePath.get(i)-1).battery));
+					}
+	    			
 	    			sensProx = true;
-	    			//System.out.println("\nReading taken as " + sensors.get(i).reading);
 	    		}
 	    	}
     	}
-    	System.out.println(drone.moves);
+    	System.out.println("\nTotal number of moves = " + drone.moves);
+    	
+    	var dronePositions = new ArrayList<Feature>();
+    	var sensorOutput = new ArrayList<List<String>>();
+    	sensorOutput.add(readings);
+    	sensorOutput.add(batteries);
+    	
+    	// Add the drone positions as a line string
+    	dronePositions.add(Feature.fromGeometry(LineString.fromLngLats(movementHistory)));
+    	System.out.println(dronePositions);
+    	// Give the lines a dark grey colour
+    	for(int j = 0; j < dronePositions.size(); j++)
+    	{
+    		dronePositions.get(j).addStringProperty("rgb-string", "#404040");
+    	}
+    	allFeatures.add(dronePositions);
+    	
+    	return sensorOutput;
     }
     
-    public static ArrayList<String> hexCodeConversion(String[] predictions)
+    public static ArrayList<List<String>> hexCodeConversion(ArrayList<List<String>> sensorOutput)
     // This function will convert the predictions into the appropriate hex-code colour
     {
     	var colourMap = new ArrayList<String>();
     	var symbolMap = new ArrayList<String>();
-    	
-    	for(int i = 0; i < 100; i++)
+    	var sensorMap = new ArrayList<List<String>>();
+    	for(int i = 0; i < sensorOutput.get(0).size(); i++)
     	{
+    		// Gray, no symbol
+    		if (Double.parseDouble(sensorOutput.get(1).get(i)) < 10)
+    		{
+    			colourMap.add(i, "#aaaaaa");
+    			symbolMap.add(i, "");
+    		}
     		// Green, lighthouse
-    		if (Integer.parseInt(predictions[i]) >= 0 && Integer.parseInt(predictions[i]) < 32)
+    		else if (Double.parseDouble(sensorOutput.get(0).get(i)) >= 0 && Double.parseDouble(sensorOutput.get(0).get(i)) < 32)
     		{
     			colourMap.add(i, "#00ff00");
-    			symbolMap.add(i, "lighthouse-15.svg");
+    			symbolMap.add(i, "lighthouse");
     		}
     		// Medium green, lighthouse
-    		else if (Integer.parseInt(predictions[i]) >= 32 && Integer.parseInt(predictions[i]) < 64)
+    		else if (Double.parseDouble(sensorOutput.get(0).get(i)) >= 32 && Double.parseDouble(sensorOutput.get(0).get(i)) < 64)
     		{
     			colourMap.add(i, "#40ff00");
+    			symbolMap.add(i, "lighthouse");
     		}
     		// Light green, lighthouse
-    		else if (Integer.parseInt(predictions[i]) >= 64 && Integer.parseInt(predictions[i]) < 96)
+    		else if (Double.parseDouble(sensorOutput.get(0).get(i)) >= 64 && Double.parseDouble(sensorOutput.get(0).get(i)) < 96)
     		{
     			colourMap.add(i, "#80ff00");
-    			symbolMap.add(i, "lighthouse-15.svg");
+    			symbolMap.add(i, "lighthouse");
     		}
     		// Lime green, lighthouse
-    		else if (Integer.parseInt(predictions[i]) >= 96 && Integer.parseInt(predictions[i]) < 128)
+    		else if (Double.parseDouble(sensorOutput.get(0).get(i)) >= 96 && Double.parseDouble(sensorOutput.get(0).get(i)) < 128)
     		{
     			colourMap.add(i, "#c0ff00");
-    			symbolMap.add(i, "lighthouse-15.svg");
+    			symbolMap.add(i, "lighthouse");
     		}
     		// Gold, danger
-    		else if (Integer.parseInt(predictions[i]) >= 128 && Integer.parseInt(predictions[i]) < 160)
+    		else if (Double.parseDouble(sensorOutput.get(0).get(i)) >= 128 && Double.parseDouble(sensorOutput.get(0).get(i)) < 160)
     		{
     			colourMap.add(i, "#ffc000");
-    			symbolMap.add(i, "danger-15.svg");
+    			symbolMap.add(i, "danger");
     		}
     		// Orange, danger
-    		else if (Integer.parseInt(predictions[i]) >= 160 && Integer.parseInt(predictions[i]) < 192)
+    		else if (Double.parseDouble(sensorOutput.get(0).get(i)) >= 160 && Double.parseDouble(sensorOutput.get(0).get(i)) < 192)
     		{
     			colourMap.add(i, "#ff8000");
-    			symbolMap.add(i, "danger-15.svg");
+    			symbolMap.add(i, "danger");
     		}
     		// Red/Orange, danger
-    		else if (Integer.parseInt(predictions[i]) >= 192 && Integer.parseInt(predictions[i]) < 224)
+    		else if (Double.parseDouble(sensorOutput.get(0).get(i)) >= 192 && Double.parseDouble(sensorOutput.get(0).get(i)) < 224)
     		{
     			colourMap.add(i, "#ff4000");
-    			symbolMap.add(i, "danger-15.svg");
+    			symbolMap.add(i, "danger");
     		}
     		// Red, danger
-    		else if (Integer.parseInt(predictions[i]) >= 224 && Integer.parseInt(predictions[i]) < 256)
+    		else if (Double.parseDouble(sensorOutput.get(0).get(i)) >= 224 && Double.parseDouble(sensorOutput.get(0).get(i)) < 256)
     		{
     			colourMap.add(i, "#ff0000");
-    			symbolMap.add(i, "danger-15.svg");
+    			symbolMap.add(i, "danger");
     		}
-    		//else if (sensor.battery < 10)
-    		//{
-    		//	colourMap.add(i, "#000000");
-    		// symbolMap.add(i, "cross-15.svg");
-    		//}
-    		//else if (notVisited)
-    		//{
-    		//	colourMap.add(i, "#aaaaaa");
-    		// symbolMap.add(i, "");
-    		//}
+    		else if (Double.parseDouble(sensorOutput.get(1).get(i)) < 10)
+    		{
+    			colourMap.add(i, "#000000");
+    			symbolMap.add(i, "cross");
+    		}
+    		
     	}
-    	return colourMap;
+    	sensorMap.add(colourMap);
+    	sensorMap.add(symbolMap);
+    	System.out.println(sensorMap.get(0).get(0));
+    	return sensorMap;
     }
-    public static void geojsonConvert(List<Polygon> polygons, ArrayList<String> colourMap) throws IOException
+    public static void geojsonConvert(ArrayList<List<String>> sensorMap, ArrayList<SensorsList> sensors, ArrayList<Integer> dronePath, ArrayList<Point> coordinates) throws IOException
     // This method will create a list of features from the polygons and their associated colour values and add
     // them to a feature collection.  This feature collection will then be converted to GeoJSON format and  
     // written to a file called 'heatmap.geojson'
     {
     	// Creating a list to store the features and a File Writer to write the geojson file
     	var featureList = new ArrayList<Feature>();
-    	var jsonFile = new FileWriter("heatmap.geojson");
-    	
+    	var jsonFile = new FileWriter("aqmaps.geojson");
+    	System.out.println(sensorMap.get(0).get(1));
     	// Loop through the feature list, adding the polygons and their colour values
-    	for(int i = 0; i < 100; i++)
+    	for(int i = 0; i < sensorMap.get(0).size()-1; i++)
     	{
-    		featureList.add(Feature.fromGeometry((Geometry)polygons.get(i)));
-    		featureList.get(i).addStringProperty("rgb-string", colourMap.get(i));
-    		featureList.get(i).addStringProperty("fill", colourMap.get(i));
-    		featureList.get(i).addNumberProperty("fill-opacity", 0.75);
+    		featureList.add(Feature.fromGeometry(coordinates.get(dronePath.get(i+1))));
+    		featureList.get(i).addStringProperty("marker-size", "medium");
+    		featureList.get(i).addStringProperty("location", sensors.get(dronePath.get(i+1)).location);
+    		featureList.get(i).addStringProperty("rgb-string", sensorMap.get(0).get(dronePath.get(i+1)));
+    		featureList.get(i).addStringProperty("marker-color", sensorMap.get(0).get(dronePath.get(i+1)));
+    		featureList.get(i).addStringProperty("marker-symbol", sensorMap.get(1).get(dronePath.get(i+1)));
     	}
     	// Create a feature collection from the list of features described above
     	var featureCol = FeatureCollection.fromFeatures(featureList);
